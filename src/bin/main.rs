3c8c43b6061f68ef74e2239880a45beba2c07f53
chlohal <email@clh.sh>
2024-07-28 11:58:45 -0400

    Use clap for args

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,34 +1,38 @@
-use std::{env::{self, args_os}, fs::File};
+use std::{env, fs::File};
 
+use clap::Parser;
 use offline_tiny_maps::compressor::Compressor;
 use osmpbfreader::blobs::result_blob_into_iter;
 
 use par_map::ParMap;
 
-
 const WRITE_EVERY_N_CHUNKS: usize = 1;
 
 fn main() {
-    let filename = args_os()
-        .nth(1)
-        .expect("Usage: tiny-map-postgres-import [OSMPBF file]");
-    let file = File::open(filename).expect("File doesn't exist!");
+    let args = Args::parse();
+
+    let file = File::open(args.osmpbf).expect("File doesn't exist!");
 
     let mut reader = osmpbfreader::OsmPbfReader::new(file);
 
-    let mut compressor = Compressor::new(env::current_dir().unwrap().join("Output"));
+    let mut compressor = Compressor::new(
+        env::current_dir()
+            .unwrap()
+            .join(args.output.unwrap_or(".map".into())),
+    );
 
     let blobs = reader
         .blobs()
         .enumerate()
-        .par_flat_map(|(i, x)| result_blob_into_iter(x).map(move |x| (i,x)));
-
+        .par_flat_map(|(i, x)| result_blob_into_iter(x).map(move |x| (i, x)));
 
     let mut last_blob_id = usize::MAX;
     let mut blobs_since_last_write = 0;
 
     for (blob_id, obj) in blobs {
-        let Ok(obj) = obj else { continue; };
+        let Ok(obj) = obj else {
+            continue;
+        };
 
         compressor.write_element(obj);
 
@@ -48,4 +52,13 @@ fn main() {
     compressor.handle_retry_queue();
 
     compressor.flush_to_storage().unwrap();
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// osm.pbf file to load
+    osmpbf: String,
+
+    /// directory to output data to. Default: `.map`
+    output: Option<String>,
 }
