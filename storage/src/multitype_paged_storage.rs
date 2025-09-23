2aac478dc069697d4e89535e6494e052d26c0eb0
chlohal <email@clh.sh>
2025-09-23 09:42:25 -0400

    feat: get a single-type subset of the multitype paged storage

--- storage/src/multitype_paged_storage.rs
+++ storage/src/multitype_paged_storage.rs
@@ -1,8 +1,14 @@
 use std::{
-    any::Any, cmp::min, io::{self, BufReader, BufWriter, Read, Write}, marker::PhantomData, ops::DerefMut, sync::{
+    any::Any,
+    cmp::min,
+    io::{self, BufReader, BufWriter, Read, Write},
+    marker::PhantomData,
+    ops::DerefMut,
+    sync::{
         atomic::{AtomicBool, AtomicUsize},
         Arc, Mutex,
-    }, thread::panicking
+    },
+    thread::panicking,
 };
 
 use debug_logs::debug_print;
@@ -59,7 +65,7 @@ impl<const K: usize, File: Filelike + 'static> MultitypePagedStorage<K, File> {
             + DeserializeFromMinimal
             + Send
             + Sync
-            + 'static
+            + 'static,
     {
         let id = self.pageuse.lock().unwrap().alloc_new();
 
@@ -94,19 +100,108 @@ impl<const K: usize, File: Filelike + 'static> MultitypePagedStorage<K, File> {
         deserialize_data: <T as DeserializeFromMinimal>::ExternalData<'b>,
     ) -> Option<Arc<Page<K, T, File>>>
     where
-        T: SerializeMinimal<ExternalData<'static> = ()> + DeserializeFromMinimal + Send + Sync + 'static,
+        T: SerializeMinimal<ExternalData<'static> = ()>
+            + DeserializeFromMinimal
+            + Send
+            + Sync
+            + 'static,
     {
         if !self.pageuse.lock().unwrap().is_valid(page_id) {
             return None;
         }
 
         let page = self.cache.get_or_insert(*page_id, || {
-            let p = Arc::new(Page::<_, T, _>::open(&self.pageuse, page_id, deserialize_data).unwrap());
+            let p =
+                Arc::new(Page::<_, T, _>::open(&self.pageuse, page_id, deserialize_data).unwrap());
             let b = p.component_pages.len() * PageId::<K>::byte_size();
             (b, p)
         });
 
         let page = page.downcast().unwrap();
+        Some(page)
+    }
+
+    pub fn single_type_view<
+        T: SerializeMinimal<ExternalData<'static> = ()>
+            + DeserializeFromMinimal
+            + Send
+            + Sync
+            + 'static,
+    >(
+        &self,
+    ) -> SingleTypeView<K, File, T> {
+        SingleTypeView {
+            pageuse: Arc::clone(&self.pageuse),
+            cache: Cache::new(ALLOWED_CACHE_PHYSICAL_PAGES * PageId::<K>::byte_size()),
+        }
+    }
+
+    pub fn flush(&self) {
+        self.cache.evict_all_possible();
+    }
+}
+
+pub struct SingleTypeView<
+    const K: usize,
+    File: Filelike + 'static,
+    T: SerializeMinimal<ExternalData<'static> = ()> + DeserializeFromMinimal + Send + Sync + 'static,
+> {
+    pageuse: Arc<Mutex<PageUse<K, File>>>,
+    cache: Cache<PageId<K>, Page<K, T, File>>,
+}
+
+impl<
+        const K: usize,
+        File: Filelike + 'static,
+        T: SerializeMinimal<ExternalData<'static> = ()>
+            + DeserializeFromMinimal
+            + Send
+            + Sync
+            + 'static,
+    > SingleTypeView<K, File, T>
+{
+    pub fn new_page(&self, item: T) -> PageId<K> {
+        let id = self.pageuse.lock().unwrap().alloc_new();
+
+        //this set will always `insert`, never `get`,
+        //because the ID was just allocated.
+        //(even in the case of reallocated pages,
+        //    they'll never be added to the pool for reallocation
+        //    until they're evicted from the cache)
+        self.cache.get_or_insert(id, || {
+            debug_print!("PagedStorage::new_page cache get_or_insert cell entered");
+
+            (
+                PageId::<K>::byte_size(),
+                Arc::new(Page {
+                    pageuse: Arc::clone(&self.pageuse),
+                    item: RwLock::new(item),
+                    dirty: true.into(),
+                    freeable: false.into(),
+                    component_pages: vec![id],
+                }),
+            )
+        });
+
+        id
+    }
+
+    pub fn get<'a, 'b>(
+        &'a self,
+        page_id: &PageId<K>,
+        deserialize_data: <T as DeserializeFromMinimal>::ExternalData<'b>,
+    ) -> Option<Arc<Page<K, T, File>>> {
+        if !self.pageuse.lock().unwrap().is_valid(page_id) {
+            return None;
+        }
+
+        let page = self.cache.get_or_insert(*page_id, || {
+            let p =
+                Arc::new(Page::<_, T, _>::open(&self.pageuse, page_id, deserialize_data).unwrap());
+            let b = p.component_pages.len() * PageId::<K>::byte_size();
+            (b, p)
+        });
+
         Some(page)
     }
 
